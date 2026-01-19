c0e9ce708ffa4b943af6bcf63373855e7222897a
Naveen <catalystack2025@gmail.com>
2026-01-19 18:15:25 +0530

    Update index.php

--- frontend/index.php
+++ frontend/index.php
@@ -1,4 +1,9 @@
 <?php
+declare(strict_types=1);
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
 include __DIR__ . '/../admin/includes/db.php';
 ?>
 <!DOCTYPE html>
